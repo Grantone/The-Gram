c8397b89a6bca46fcf0eb1f01799f3457a167946
Granton Mchana <gmchana254@gmail.com>
2017-11-25 15:27:38 +0300

    fixing bugs in view file

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User)
+    post_image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    title = models.CharField(max_length=60)
+
+    def get_number_of_likes(self):
+        return self.like_set.count()
+
+    def get_number_of_comments(self):
+        return self.comment_set.count()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post')
+    comment = models.CharField(max_length=100)
--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,51 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
-def welcome(request):
-    return render(request, 'welcome.html')
+def index(request):
+    # return render(request, 'all-posts/index.html')
+    if not request.user.is_authenticated():
+        redirect('login')
+
+    users_followed = request.user.userprofile.following.all()
+    posts = IGPost.objects.filter(
+        user_profile__in=users_followed).order_by('-posted_on')
+
+    return render(request, 'all-posts/index.html', {
+        'posts': posts
+    })
+
+
+def post(request, pk):
+    post = Post.objects.get(pk=pk)
+    try:
+        like = Like.objects.get(post=post, user=request.user)
+        liked = 1
+    except:
+        like = None
+        liked = 0
+
+    context = {
+        'post': post,
+        'liked': liked
+    }
+    return render(request, 'all-posts/post.html', context)
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    if not user:
+        return redirect('index')
+
+    profile = UserProfile.objects.get(user=user)
+    context = {
+        'username': username,
+        'user': user,
+        'profile': profile
+    }
+    return render(request, 'all-posts/profile.html', context)
