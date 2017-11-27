119940695fe1ae7ead39d19849538dd7e8364dce
Granton Mchana <gmchana254@gmail.com>
2017-11-27 12:51:48 +0300

    working on authentication

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -11,3 +14,6 @@ urlpatterns = [
     #     views.following, name='following'),
     url(r'^profile/', views.profile, name='profile'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
